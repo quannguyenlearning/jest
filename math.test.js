df18a747661f769cb7acfaeb67b9acc108e4d52c
Quan Nguyen <quannguyentxus@gmail.com>
2019-02-14 16:13:37 -0600

    Refactor, and add substract function

---
+++ math.test.js
@@ -0,0 +1,9 @@
+const math = require('./math');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(math.add(1, 2)).toBe(3);
+});
+
+test('subtract 3 by 1 equals 2', () => {
+    expect(math.subtract(3, 1)).toBe(2);
+})
