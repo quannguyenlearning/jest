75b60f54f079b13ecd1662954896cb101d5dcc89
Quan Nguyen <quannguyentxus@gmail.com>
2019-02-15 13:18:47 -0600

    Mock a function

--- math.test.js
+++ math.test.js
@@ -1,4 +1,5 @@
 const math = require('./math');
+const mathLib = require('mathjs');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(math.add(1, 2)).toBe(3);
@@ -8,6 +9,19 @@ test('subtract 3 by 1 equals 2', () => {
     expect(math.subtract(3, 1)).toBe(2);
 })
 
-test('sqrt of 4 is 2', () => {
-    expect(math.sqrt(4)).toBe(2);
+describe('Mock test', () => {
+    // Mock some functions of the external math library
+    mathLib.sqrt = jest.fn();
+
+    test('sqrt of 4 is 2', () => {
+        /// Arrange
+        mathLib.sqrt.mockReturnValue(2);
+
+        /// Action
+        let returnedValue = math.sqrt(4);
+
+        /// Assert
+        expect(mathLib.sqrt).toHaveBeenCalledWith(4);
+        expect(returnedValue).toBe(2);
+    })
 })
